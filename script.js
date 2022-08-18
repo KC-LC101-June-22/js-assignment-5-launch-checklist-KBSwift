00e6f685587722a901984a2fc80a1c893d939f36
Kavin Moreno <kavinmoreno@gmail.com>
2022-08-18 17:56:34 -0500

    testing validation

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    let form = document.querySelector("#launchForm");
+    form.addEventListener("#launchForm", function(event) {
+        let pilotNameInput = document.querySelector("#pilotName");
+        if (pilotNameInput.value === "") {
+            alert("All fields are required!");
+        }
+    });
+    
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse;
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    })
+
 });
