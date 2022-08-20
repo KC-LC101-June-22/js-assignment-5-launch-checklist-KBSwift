7c79f005d07d0db1fda33c5389cbedec4910e2d2
Kavin Moreno <kavinmoreno@gmail.com>
2022-08-20 00:21:29 -0500

    error with final test?

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ window.addEventListener("load", function () {
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
         let planet = pickPlanet(listedPlanets);
         addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
-        this.document.querySelector("#faultyItems").style.visibility = "hidden";
+        this.document.querySelector("#faultyItems").style.visibility = "hidden"; //not working for failing test
     })
 
 });
