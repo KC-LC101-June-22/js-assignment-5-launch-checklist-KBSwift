c75e988501be2c094561726dc08b09ac95e01387
Kavin Moreno <kavinmoreno@gmail.com>
2022-08-19 22:16:10 -0500

    More testing

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-// const { formSubmission } = require("./scriptHelper.js");
+const { formSubmission } = require("./scriptHelper.js");
 
 window.addEventListener("load", function () {
 
-    this.document.querySelector("#faultyItems").style.visibility = "hidden";
+    const faultyVisibility = document.querySelector("#faultyItems");
+    faultyVisibility.style.visibility = "hidden";
     const form = document.querySelector("#launchForm");
     form.addEventListener("submit", function(event) {
         event.preventDefault();
