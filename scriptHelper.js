aab265d3b99949ba40973939860e90541f758251
Kavin Moreno <kavinmoreno@gmail.com>
2022-09-01 19:34:24 -0500

    attempting to pass all tests

--- scriptHelper.js
+++ scriptHelper.js
@@ -52,42 +52,42 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         list.style.visibility = "hidden";
     }
 
-    if (fuelLevel > 10000 && cargoLevel < 10000) {
-        launchStatus.innerHTML = "Shuttle is Ready for Launch";
-        launchStatus.style.color = "rgb(65, 159, 106)";
-        fuelStatus.innerHTML = `Fuel level high enough for launch`;
-        cargoStatus.innerHTML = `Cargo mass low enough for launch`;
-        list.style.visibility = "visible";
-    } else if (cargoLevel >= 10000 || fuelLevel < 10000) {
-        list.style.visibility = "visible";
-        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-        launchStatus.style.color = "rgb(199, 37, 78)";
-        fuelStatus.innerHTML = `Fuel level too low for launch`;
-        fuelStatus.style.color = "rgb(199, 37, 78)";
-        cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
-        cargoStatus.style.color = "rgb(199, 37, 78)";
-    }
-
-    // if (fuelLevel < 10000) {
-    //     list.style.visibility = "visible";
-    //     launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-    //     launchStatus.style.color = "rgb(199, 37, 78)";
-    //     fuelStatus.innerHTML = `Fuel level too low for launch`;
-    //     fuelStatus.style.color = "rgb(199, 37, 78)"
-    // } else if (cargoLevel > 10000) {
-    //     list.style.visibility = "visible";
-    //     launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-    //     launchStatus.style.color = "rgb(199, 37, 78)";
-    //     fuelStatus.innerHTML = `Fuel level high enough for launch`;
-    //     cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
-    //     cargoStatus.style.color = "rgb(199, 37, 78)";
-    // } else {
+    // if (fuelLevel > 10000 && cargoLevel < 10000) {
     //     launchStatus.innerHTML = "Shuttle is Ready for Launch";
     //     launchStatus.style.color = "rgb(65, 159, 106)";
     //     fuelStatus.innerHTML = `Fuel level high enough for launch`;
     //     cargoStatus.innerHTML = `Cargo mass low enough for launch`;
     //     list.style.visibility = "visible";
+    // } else if (cargoLevel >= 10000 || fuelLevel < 10000) {
+    //     list.style.visibility = "visible";
+    //     launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+    //     launchStatus.style.color = "rgb(199, 37, 78)";
+    //     fuelStatus.innerHTML = `Fuel level too low for launch`;
+    //     fuelStatus.style.color = "rgb(199, 37, 78)";
+    //     cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
+    //     cargoStatus.style.color = "rgb(199, 37, 78)";
     // }
+
+    if (fuelLevel < 10000) {
+        list.style.visibility = "visible";
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+        launchStatus.style.color = "rgb(199, 37, 78)";
+        fuelStatus.innerHTML = `Fuel level too low for launch`;
+        fuelStatus.style.color = "rgb(199, 37, 78)"
+    } else if (cargoLevel > 10000) {
+        list.style.visibility = "visible";
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+        launchStatus.style.color = "rgb(199, 37, 78)";
+        fuelStatus.innerHTML = `Fuel level high enough for launch`;
+        cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
+        cargoStatus.style.color = "rgb(199, 37, 78)";
+    } else {
+        launchStatus.innerHTML = "Shuttle is Ready for Launch";
+        launchStatus.style.color = "rgb(65, 159, 106)";
+        fuelStatus.innerHTML = `Fuel level high enough for launch`;
+        cargoStatus.innerHTML = `Cargo mass low enough for launch`;
+        list.style.visibility = "visible";
+    }
 
 }
 
